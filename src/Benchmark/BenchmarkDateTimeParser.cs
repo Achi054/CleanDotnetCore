00ba5aa8cc34dd1bc7d1cd8c05ba1c234d93ec6c
Sujith Acharya <sujithacharya@eurofins.com>
2020-01-30 16:21:37 +0530

    Working with BenchmarkDotNet

---
+++ src/Benchmark/BenchmarkDateTimeParser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+
+namespace Benchmark
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    [Orderer(BenchmarkDotNet.Order.SummaryOrderPolicy.FastestToSlowest)]
+    public class BenchmarkDateTimeParser
+    {
+        private readonly string datetime = DateTime.UtcNow.ToString("yyyy-MM-ddThh:mm:ssZ");
+        private readonly DateTimeParser _parser = new DateTimeParser();
+
+        [Benchmark(Baseline = true)]
+        public void Benchmar_GetYearFromDate()
+        {
+            _parser.GetYearFromDate(datetime);
+        }
+
+        [Benchmark]
+        public void Benchmar_GetYearFromDateSplit()
+        {
+            _parser.GetYearFromDateSplit(datetime);
+        }
+
+        [Benchmark]
+        public void Benchmar_GetYearFromDateSubstring()
+        {
+            _parser.GetYearFromDateSubstring(datetime);
+        }
+
+        [Benchmark]
+        public void Benchmar_GetYearFromDateAsSpan()
+        {
+            _parser.GetYearFromDateAsSpan(datetime);
+        }
+    }
+}
---
+++ src/Benchmark/DateTimeParser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Benchmark
+{
+    internal class DateTimeParser
+    {
+        internal int GetYearFromDate(string datetime)
+        {
+            var datetimeInfo = DateTime.Parse(datetime);
+            return datetimeInfo.Year;
+        }
+
+        internal int GetYearFromDateSplit(string datetime)
+        {
+            var dateTimeCollection = datetime.Split('-');
+            return int.Parse(dateTimeCollection[0]);
+        }
+
+        internal int GetYearFromDateSubstring(string datetime)
+        {
+            var index = datetime.IndexOf('-');
+            return int.Parse(datetime.Substring(0, index));
+        }
+
+        internal int GetYearFromDateAsSpan(ReadOnlySpan<char> datetimecollection)
+        {
+            var index = datetimecollection.IndexOf('-');
+            return int.Parse(datetimecollection.Slice(0, index));
+        }
+    }
+}
