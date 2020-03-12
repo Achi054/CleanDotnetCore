0ce6be40c43d36a5a4cf4a0045c9a020e0f3c33c
Sujith Acharya <sujithacharya@eurofins.com>
2020-03-12 16:00:32 +0530

    Api Authorization

---
+++ src/OrderApi/Controllers/SecretController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrderApi.Controllers
+{
+    public class SecretController : ControllerBase
+    {
+        [HttpGet("secret")]
+        public IActionResult Get() => Ok("I have no secret !!");
+    }
+}
